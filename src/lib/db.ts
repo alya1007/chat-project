d2ff437a5ad484d830e0566c0d6387766c04f621
alya1007 <alexandra.konjevic@isa.utm.md>
2023-06-27 13:06:19 +0300

    config

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 import { Redis } from "@upstash/redis";
 
 export const db = new Redis({
-  url: "https://pleasing-gazelle-39513.upstash.io",
+  url: process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_URL,
   token:
     "AZpZASQgMzczYmQ0OTYtMDAyMC00MmExLTgwMTEtZGY0MmRjMTVhODIwNzY1Mjk4ZmM1ZmFhNGUzNTg5OTQ1MzA4NjNiNGRhYzY=",
 });
