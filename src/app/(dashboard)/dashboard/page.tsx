8f8c53dc68137cd91df6f1c03ffec24548c34c94
alya1007 <alexandra.konjevic@isa.utm.md>
2023-06-28 21:23:23 +0300

    add friend form

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Button from "@/components/ui/Button";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+interface pageProps {}
+
+const page = async ({}) => {
+  const session = await getServerSession(authOptions);
+  return <pre>{JSON.stringify(session)}</pre>;
+};
+
+export default page;
