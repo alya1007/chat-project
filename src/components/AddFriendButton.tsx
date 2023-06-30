46b713f96ae3515833b8597f4e918f5f89b4c514
alya1007 <alexandra.konjevic@isa.utm.md>
2023-06-30 15:56:48 +0300

    add friend validator

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -1,28 +1,62 @@
 "use client";
 
-import { FC } from "react";
+import { FC, useState } from "react";
 import Button from "./ui/Button";
+import { addFriendValidator } from "@/lib/validations/add-friend";
+import axios from "axios";
+import { ZodRecord, z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface AddFriendButtonProps {}
+
+type FormData = z.infer<typeof addFriendValidator>;
+
 const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
-  return (
-    <form action="" className="max-w-sm">
-      <label
-        htmlFor="emial"
-        className="block text-sm font-md leading-6 text-gray-900"
-      >
-        Add friend by email
-      </label>
-      <div className="mt-2 flex gap-4">
-        <input
-          type="text"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          placeholder="email@example.com"
-        />
-        <Button>button</Button>
-      </div>
-    </form>
-  );
+	const [showSuccessState, setShowSuccessState] = useState<boolean>(false);
+	const { register, handleSubmit, setError } = useForm<FormData>({
+		resolver: zodResolver(addFriendValidator),
+	});
+
+	const addFriend = async (email: string) => {
+		try {
+			const validatedEmail = addFriendValidator.parse({ email });
+			await axios.post("/api/friends/add", {
+				email: validatedEmail,
+			});
+			setShowSuccessState(true);
+		} catch (error) {
+			if (error instanceof z.ZodError) {
+				setError("email", { message: error.message });
+				return;
+			}
+
+			if (error instanceof axios.AxiosError) {
+				setError("email", { message: error.response?.data });
+
+				return;
+			}
+		}
+	};
+	return (
+		<form action="" className="max-w-sm">
+			<label
+				htmlFor="emial"
+				className="block text-sm font-md leading-6 text-gray-900"
+			>
+				Add friend by email
+			</label>
+			<div className="mt-2 flex gap-4">
+				<input
+					{...register("email")}
+					type="text"
+					className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+					placeholder="email@example.com"
+				/>
+				<Button>Add</Button>
+			</div>
+		</form>
+	);
 };
 
 export default AddFriendButton;
