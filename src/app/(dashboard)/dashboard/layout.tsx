0986f21ed2f0b50a5cda197a47bfeb6b539932d2
alya1007 <alexandra.konjevic@isa.utm.md>
2023-07-01 16:05:19 +0300

    layout dashboard

---
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,33 @@
+import { authOptions } from "@/lib/auth";
+import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import { FC, ReactNode } from "react";
+import { Icons } from "@/components/Icons";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = async ({ children }: LayoutProps) => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) notFound();
+
+  return (
+    <div className="w-full flex h-screen">
+      <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+        <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
+          <Icons.Logo className="h-8 w-auto text-indigo-600" />
+        </Link>
+
+        <div className="text-sx font-semibold leading-6 text-gray-400">
+          Your chats
+        </div>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
